3945142cdfef78fc948418322f3cd2492bcbc871
Zhanaidar Mukanov <zhanaidar@Zhanaidars-MacBook-Pro.local>
2021-07-29 18:44:48 +0800

    build function

--- quadtree.cpp
+++ quadtree.cpp
@@ -2,6 +2,56 @@
 #include <stack>
 using namespace std;
 
+char matrix[1025][1025];
+
+struct Node {
+    int answer;
+    int sum;
+    int level;
+    Node *ul, *ur, *dl, *dr;
+
+    Node() {
+        answer = sum = level = 0;
+        ul = ur = dl = dr = NULL;
+    }
+};
+
+Node* build(int l, int r, int u, int d, int lvl) {
+    Node *answer = new Node();
+
+    answer -> answer = 1;
+    answer -> level = lvl;
+
+    if (l == r) {
+        answer -> sum = matrix[u][r] - '0';
+    } else {
+
+        int m1 = (l + r) / 2;
+        int m2 = (u + d) / 2;
+        answer->ul = build(l, m1, u, m2, lvl - 1);
+        answer->ur = build(m1 + 1, r, u, m2, lvl - 1);
+        answer->dl = build(l, m1, m2 + 1, d, lvl - 1);
+        answer->dr = build(m1 + 1, r, m2 + 1, d, lvl - 1);
+
+        answer->sum += answer->ul->sum;
+        answer->sum += answer->ur->sum;
+        answer->sum += answer->dl->sum;
+        answer->sum += answer->dr->sum;
+
+        if (answer->sum == 0 || answer->sum == (1 << answer->level) * (1 << answer->level)) {
+            answer->answer = 1;
+        } else {
+            answer->answer = 1;
+            answer->answer += answer->ul->answer;
+            answer->answer += answer->ur->answer;
+            answer->answer += answer->dl->answer;
+            answer->answer += answer->dr->answer;
+        }
+    }
+    return answer;
+}
+
+
 int main() {
 
     int T;
@@ -13,11 +63,11 @@ int main() {
         cin >> k;
 
         int n = 1;
-        for (int i = 0; i <= k; ++i) {
+        for (int i = 0; i < k; ++i) {
             n *= 2;
         }
         
-        int matrix[1025][1025];
+        
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
@@ -25,12 +75,19 @@ int main() {
             }
         }
 
+        auto root = build(1, n, 1, n, k);
+
         int m;
         cin >> m;
         
         for (int i = 0, r, c; i < m; ++i) {
             cin >> r >> c;
+
+            update(root, c, r, 1, m, 1, m);
+            cout << root -> answer << endl;
         }
+
+        clean(root);
 
     }
 
