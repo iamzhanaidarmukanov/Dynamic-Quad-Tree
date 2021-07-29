d8a1f5077e7c855457b929d2fb3394f5aaba44a6
Zhanaidar Mukanov <zhanaidar@Zhanaidars-MacBook-Pro.local>
2021-07-29 18:52:17 +0800

    update function

--- quadtree.cpp
+++ quadtree.cpp
@@ -51,6 +51,38 @@ Node* build(int l, int r, int u, int d, int lvl) {
     return answer;
 }
 
+void update(node *answer, int x, int y, int l, int r, int u, int d) {
+
+    answer->answer = 1;
+    if (l == r) {
+        answer->sum ^= 1;
+    } else {
+        int m1 = (l + r) / 2;
+        int m2 = (u + d) / 2;
+        
+        if (x <= m1 && y <= m2) update(answer->ul, x, y, l, m1, u, m2);
+        else if (x <= m1 && y > m2) update(answer->ur, x, y, m1 + 1, r, u, m2);
+        else if (x > m1 && y <= m2) update(answer->dl, x, y, l, m1, m2 + 1, d);
+        else if (x > m1 && y > m2) update(answer->dr, x, y, m1 + 1, r, m2 + 1, d);
+
+        answer->sum = 0;
+        answer->sum += answer->ul->sum;
+        answer->sum += answer->ur->sum;
+        answer->sum += answer->dl->sum;
+        answer->sum += answer->dr->sum;
+
+        if (answer->sum == 0 || answer->sum == (1 << answer->level) * (1 << answer->level)) {
+            answer->answer = 1;
+        } else {
+            answer->answer = 1;
+            answer->answer += answer->ul->answer;
+            answer->answer += answer->ur->answer;
+            answer->answer += answer->dl->answer;
+            answer->answer += answer->dr->answer;
+        }
+    }
+}
+
 
 int main() {
 
